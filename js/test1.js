d771ba390e4e1850e73f8b8fbd71af7cdb9bf879
MILLKA3000 <milenium.sergiy@gmail.com>
2015-04-24 15:23:34 +0300

    upd

---
+++ js/test1.js
@@ -0,0 +1,8 @@
+var App = angular.module('myApp', []);
+
+App.controller('Ctrl', function ($scope) {
+    $scope.empty = "Please enter word";
+    $scope.cx = 5;
+    $scope.mycolor = "green";
+
+});
